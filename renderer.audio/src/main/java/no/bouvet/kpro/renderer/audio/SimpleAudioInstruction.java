573adb26ce393ef6bca22fe67a43568d6b96efad
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2008-03-27 23:32:23 +0000

    added SimpleAudioInstruction

---
+++ renderer.audio/src/main/java/no/bouvet/kpro/renderer/audio/SimpleAudioInstruction.java
@@ -0,0 +1,27 @@
+package no.bouvet.kpro.renderer.audio;
+
+import no.bouvet.kpro.renderer.Renderer;
+
+public class SimpleAudioInstruction extends AudioInstruction {
+
+    public SimpleAudioInstruction(int start, int end, float bpm, float cue, AudioSource source) {
+        this(start, end, bpm, cue, 0f, source);
+    }
+
+    public SimpleAudioInstruction(int start, int end, float bpm, float cue, float initialOffset, AudioSource source) {
+        float speedFactor = 44100 * 60 / bpm;
+
+        _start = new Float(start * speedFactor).intValue() ;
+        _end = new Float(end * speedFactor).intValue();
+
+
+        _cue = new Float((cue*speedFactor) + initialOffset).intValue();
+        _duration = _end - _start;
+
+        _source = source;
+        _rate1 = Renderer.RATE;
+		_rated = 0;
+		_volume1 = 127;
+		_volumed = 0;
+    }
+}
