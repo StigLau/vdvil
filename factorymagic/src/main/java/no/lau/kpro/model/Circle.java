80c80fd0c550549e2b9ed431b4f3822d11b07ac0
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2008-07-12 21:25:45 +0000

    Testing Oval with Squares and Circles

---
+++ factorymagic/src/main/java/no/lau/kpro/model/Circle.java
@@ -0,0 +1,8 @@
+package no.lau.kpro.model;
+
+/**
+ * Author: <a href="mailto:stig@lau.no">Stig Lau</a>
+ * Date: Jul 6, 2008
+ */
+public class Circle implements Shape {
+}
---
+++ factorymagic/src/main/java/no/lau/kpro/model/Square.java
@@ -0,0 +1,8 @@
+package no.lau.kpro.model;
+
+/**
+ * Author: <a href="mailto:stig@lau.no">Stig Lau</a>
+ * Date: Jul 3, 2008
+ */
+public class Square implements Shape {
+}
