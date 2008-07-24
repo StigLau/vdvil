927fb86287cd8d178b9e1f5861a84c149c4427f3
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2008-07-24 06:34:38 +0000

    Used InstanceOfAny validation

--- factorymagic/src/main/java/no/lau/kpro/model/CircleReceptor.java
+++ factorymagic/src/main/java/no/lau/kpro/model/CircleReceptor.java
@@ -3,7 +3,6 @@ package no.lau.kpro.model;
 import no.lau.kpro.domain.Receptor;
 import no.lau.kpro.domain.TopicIdentifyable;
 import net.sf.oval.constraint.*;
-import net.sf.oval.guard.Pre;
 
 /**
  * Author: <a href="mailto:stig@lau.no">Stig Lau</a>
@@ -13,8 +12,7 @@ import net.sf.oval.guard.Pre;
 public class CircleReceptor implements Receptor {
 
 
-    @Pre(expr = "return _args[0] instanceof no.lau.kpro.model.Circle", lang = "groovy")
-    public void put(@NotNull TopicIdentifyable shape) {
+    public void put(@NotNull @InstanceOfAny(value = Circle.class) TopicIdentifyable shape) {
         System.out.println("Circle receptor invoked " + shape);
     }
 
--- factorymagic/src/main/java/no/lau/kpro/model/SquareReceptor.java
+++ factorymagic/src/main/java/no/lau/kpro/model/SquareReceptor.java
@@ -3,7 +3,6 @@ package no.lau.kpro.model;
 import no.lau.kpro.domain.Receptor;
 import no.lau.kpro.domain.TopicIdentifyable;
 import net.sf.oval.constraint.*;
-import net.sf.oval.guard.Pre;
 
 /**
  * Author: <a href="mailto:stig@lau.no">Stig Lau</a>
@@ -13,8 +12,7 @@ import net.sf.oval.guard.Pre;
 public class SquareReceptor implements Receptor {
 
 
-    @Pre(expr = "return _args[0] instanceof no.lau.kpro.model.Square", lang = "groovy")
-    public void put(@NotNull TopicIdentifyable shape) {
+    public void put(@NotNull @InstanceOfAny(value = Square.class) TopicIdentifyable shape) {
         System.out.println("Sqaure receptor invoked " + shape);
     }
 
