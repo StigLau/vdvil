799b5bcdf4dd7286f5403c4cae5d4922912fa9c1
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2008-09-16 13:31:55 +0000

    JavaZoneTest is ready for prod

--- tagger/src/test/java/no/bouvet/kpro/mix/JavaZoneTest.java
+++ tagger/src/test/java/no/bouvet/kpro/mix/JavaZoneTest.java
@@ -32,62 +32,28 @@ public class JavaZoneTest {
 
 
     public void testPlayingSomeStuff() throws Exception {
-        MasterSong master = new MasterSong();
-        /*
-        master.setMasterBpm(130F);
-        master.addPart(createPart(unfinished_sympathy, 0F, 16F, unfinished_sympathy.rows.get(3)));
-        master.addPart(createPart(unfinished_sympathy, 16F, 32F, unfinished_sympathy.rows.get(6)));
-        master.addPart(createPart(returning, 32F, 48F, returning.rows.get(6)));
-        master.addPart(createPart(unfinished_sympathy, 32F, 64F, unfinished_sympathy.rows.get(8)));
-*/
-        master.setMasterBpm(160F);
-        master.addPart(createPart(not_alone, 0F, 32F, not_alone.rows.get(0)));
-        master.addPart(createPart(scares_me, 16F, 48F, scares_me.rows.get(2)));
-        master.addPart(createPart(not_alone, 32F, 70F, not_alone.rows.get(1)));
-        master.addPart(createPart(scares_me, 48F, 64F, scares_me.rows.get(2)));
-        master.addPart(createPart(scares_me, 64F, 112F, scares_me.rows.get(4)));
+        MasterSong composition = new MasterSong();
 
-        master.addPart(createPart(returning, 96F, 140F, returning.rows.get(4)));
-        master.addPart(createPart(returning, 96F, 140F, returning.rows.get(4)));
+        composition.setMasterBpm(150F);
+        composition.addPart(createPart(not_alone, 0F, 32F, not_alone.rows.get(0)));
+        composition.addPart(createPart(scares_me, 16F, 48F, scares_me.rows.get(2)));
+        composition.addPart(createPart(not_alone, 32F, 70F, not_alone.rows.get(1)));
+        composition.addPart(createPart(scares_me, 48F, 64F, scares_me.rows.get(2)));
+        composition.addPart(createPart(scares_me, 64F, 112F, scares_me.rows.get(4)));
+        composition.addPart(createPart(returning, 96F, 140F, returning.rows.get(4)));
+        composition.addPart(createPart(returning, 96F, 140F, returning.rows.get(4)));
+        composition.addPart(createPart(returning, 128F, 174F, returning.rows.get(6)));
+        composition.addPart(createPart(returning, 144F, 174.5F, returning.rows.get(9)));
+        composition.addPart(createPart(returning, 174F, 175.5F, returning.rows.get(10)));
+        composition.addPart(createPart(returning, 175F, 176.5F, returning.rows.get(11)));
+        composition.addPart(createPart(returning, 176F, 240F, returning.rows.get(12)));
+        composition.addPart(createPart(scares_me, 208F, 224F, scares_me.rows.get(12)));
+        composition.addPart(createPart(scares_me, 224F, 252F, scares_me.rows.get(13)));
 
-        master.addPart(createPart(returning, 128F, 174F, returning.rows.get(6)));
-        master.addPart(createPart(returning, 144F, 174.5F, returning.rows.get(9)));
-        master.addPart(createPart(returning, 174F, 175.5F, returning.rows.get(10)));
-        master.addPart(createPart(returning, 175F, 176.5F, returning.rows.get(11)));
-        master.addPart(createPart(returning, 176F, 240F, returning.rows.get(12)));
-
-        master.addPart(createPart(scares_me, 208F, 224F, scares_me.rows.get(12)));
-        master.addPart(createPart(scares_me, 224F, 252F, scares_me.rows.get(13)));
-        //master.addPart(createPart(not_alone, 240F, 256F, not_alone.rows.get(7)));
-        //master.addPart(createPart(not_alone, 271F, 296F, not_alone.rows.get(9)));
-
-        
-        //master.addPart(createPart(unfinished_sympathy, 240F, 300F, unfinished_sympathy.rows.get(7)));
-
-
-
-
-        //master.addPart(createPart(scares_me, 32F, 64F, scares_me.rows.get(2)));
-        //master.addPart(createPart(scares_me, 32F, 64F, scares_me.rows.get(4)));
-        //master.addPart(createPart(not_alone, 64F, 128F, not_alone.rows.get(3)));
-        //master.addPart(createPart(scares_me, 64F, 128F, scares_me.rows.get(4)));
-
-
-
-        /*
-        master.addPart(createPart(returning, 16F, 32F, returning.rows.get(6)));
-        master.addPart(createPart(returning, 32F, 62.5F, returning.rows.get(9)));
-        master.addPart(createPart(returning, 62F, 63.5F, returning.rows.get(10)));
-        master.addPart(createPart(returning, 63F, 64.5F, returning.rows.get(11)));
-        master.addPart(createPart(returning, 64F, 128F, returning.rows.get(12)));
-        */
-        //master.addPart(createPart(returning, 128F, 256F, returning.rows.get(14)));
-
-
-        PlayStuff playStuff = new PlayStuff();
-        playStuff.setMasterSong(master);
-        playStuff.init();
-        playStuff.play(0F);
+        PlayStuff player = new PlayStuff();
+        player.setMasterSong(composition);
+        player.init();
+        player.play(0F);
     }
 
     private Part createPart(SimpleSong ss, Float start, Float end, Row row) {
