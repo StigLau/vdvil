9684c065c131b7fabda22971add09844c45cb84f
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2010-01-03 17:54:09 +0000

    Moved PlayingStuffDemo to tests

--- tagger-gui/src/test/java/no/bouvet/kpro/tagger/PlayingStuffDemo.java
+++ tagger-gui/src/test/java/no/bouvet/kpro/tagger/PlayingStuffDemo.java
@@ -16,7 +16,7 @@ public class PlayingStuffDemo {
     public static void main(String[] args) {
         PlayingStuffDemo test = new PlayingStuffDemo();
         test.beforeMethod();
-        PlayStuff playStuff = test.createPlayStuff();
+        PlayStuff playStuff = new PlayStuff(new Composition(160F, test.parts()));
         try {
             playStuff.init();
             playStuff.play(0F);
@@ -29,19 +29,9 @@ public class PlayingStuffDemo {
         XStreamParser parser = new XStreamParser();
         returning = parser.load("/Users/stiglau/kpro/holden-nothing-93_returning_mix.dvl");
         unfinished_sympathy = parser.load("/Users/stiglau/kpro/unfinished_sympathy.dvl");
-
     }
 
-
-    public PlayStuff createPlayStuff(){
-
-        /*
-        master.setMasterBpm(130F);
-        master.addPart(createPart(unfinished_sympathy, 0F, 16F, unfinished_sympathy.segments.get(3)));
-        master.addPart(createPart(unfinished_sympathy, 16F, 32F, unfinished_sympathy.segments.get(6)));
-        master.addPart(createPart(returning, 32F, 48F, returning.segments.get(6)));
-        master.addPart(createPart(unfinished_sympathy, 32F, 64F, unfinished_sympathy.segments.get(8)));
-*/
+    public List<Part> parts(){
         List<Part> parts = new ArrayList<Part>();
         parts.add(new Part(returning, 0F, 16F, returning.segments.get(3)));
         parts.add(new Part(returning, 12F, 32F, returning.segments.get(6)));
@@ -50,7 +40,6 @@ public class PlayingStuffDemo {
         parts.add(new Part(returning, 63F, 64.5F, returning.segments.get(11)));
         parts.add(new Part(returning, 64F, 128F, returning.segments.get(12)));
         parts.add(new Part(returning, 128F, 256F, returning.segments.get(14)));
-
-        return new PlayStuff(new Composition(160F, parts));
+        return parts;
     }
 }
