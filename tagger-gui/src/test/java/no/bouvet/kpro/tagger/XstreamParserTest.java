9c789adb65a5cd8f17659abe5b8537321aa099a1
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2010-01-03 13:12:48 +0000

    Removed unused Class  - SimpleSongParser/Test

--- tagger-gui/src/test/java/no/bouvet/kpro/tagger/XstreamParserTest.java
+++ tagger-gui/src/test/java/no/bouvet/kpro/tagger/XstreamParserTest.java
@@ -1,16 +1,21 @@
 package no.bouvet.kpro.tagger;
 
+import no.lau.tagger.model.MediaFile;
+import no.lau.tagger.model.Segment;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import no.lau.tagger.model.SimpleSong;
 import no.bouvet.kpro.tagger.persistence.XStreamParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class XstreamParserTest {
     XStreamParser parser = new XStreamParser();
 
     @Test
     public void parseWithXStream() {
-        SimpleSong joe = SimpleSongParserTest.coronaTest();
+        SimpleSong joe = coronaTest();
 
         //Serialize
         String xml = parser.toXml(joe);
@@ -22,9 +27,26 @@ public class XstreamParserTest {
 
     @Test
     public void writeWithXstreamParser() {
-        String xml = parser.toXml(SimpleSongParserTest.coronaTest());
+        String xml = parser.toXml(coronaTest());
         
         SimpleSong simpleSong = parser.fromXML(xml);
         Assert.assertEquals(simpleSong.mediaFile.fileName, "/Users/stiglau/jobb/utvikling/bouvet/playground/stig.lau/kpro2007/renderer.audio/src/test/resources/Corona_-_Baby_Baby.mp3");
+    }
+
+    public static SimpleSong coronaTest() {
+        MediaFile mediaFile = new MediaFile("/Users/stiglau/jobb/utvikling/bouvet/playground/stig.lau/kpro2007/renderer.audio/src/test/resources/Corona_-_Baby_Baby.mp3",
+                44100 * 0.445f);
+        List<Segment> segments = new ArrayList<Segment>();
+        segments.add(new Segment("a", 0F, 16F,  "Baby, why can't we just stay together"));
+        segments.add(new Segment("b", 16F, 32F,  "Baby, why can't we just stay together"));
+        segments.add(new Segment("c", 32F, 64F,  "Intro"));
+        segments.add(new Segment("d", 64F, 96F,  "Riff 1. time"));
+        segments.add(new Segment("e", 96F, 128F,  "1. Refrain  I want to roll inside your soul,"));
+        segments.add(new Segment("f", 128F, 128 + 32F,  "2. Verse - Caught you down by suprise"));
+        segments.add(new Segment("g", 128 + 32F, 128 + 64F,  "Baby baby, why can't we just stay together"));
+        segments.add(new Segment("h", 128 + 64F, 128 + 96F, "riff 2. time"));
+        segments.add(new Segment("i", 128 + 96F, 256F,  "Deep inside I know you need it"));
+        segments.add(new Segment("j", 256F, 256 + 32F, "Caught you down by suprise"));
+        return new SimpleSong("Corona - Baby baby", mediaFile, segments, 132.98f);
     }
 }
