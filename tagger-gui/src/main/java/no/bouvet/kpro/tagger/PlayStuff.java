561b413a4ff9e02fd72a221fa040d113df167511
stig.lau <stig.lau@ed770290-9dff-0310-b147-c19b6f09e32a>
2010-01-05 08:11:03 +0000

    Ready for refactoring

--- tagger-gui/src/main/java/no/bouvet/kpro/tagger/PlayStuff.java
+++ tagger-gui/src/main/java/no/bouvet/kpro/tagger/PlayStuff.java
@@ -1,12 +1,15 @@
 package no.bouvet.kpro.tagger;
 
+import no.bouvet.kpro.renderer.*;
 import no.bouvet.kpro.renderer.audio.*;
-import no.bouvet.kpro.renderer.Instructions;
-import no.bouvet.kpro.renderer.Renderer;
 import no.lau.tagger.model.Composition;
+import no.lau.tagger.model.LyricPart;
 import no.lau.tagger.model.Part;
 import java.io.File;
 
+/**
+ * This is the master class, responsible for playing a small demoset of VDVIL music
+ */
 public class PlayStuff {
     private final Renderer renderer;
     private final Float masterBpm;
@@ -21,20 +24,16 @@ public class PlayStuff {
     public static Instructions createInstructionsFromParts(Composition composition) throws Exception {
         Instructions instructions = new Instructions();
         for (Part part : composition.parts) {
-            AudioSource audioSource = new MP3Source(new File(part.simpleSong.mediaFile.fileName));
             //TODO check why diff neeeds to be opposite
             Float partCompositionDiff = part.bpm / composition.masterBpm;
             Float compositionPartDiff = composition.masterBpm / part.bpm;
-            AudioInstruction audioInstruction = new SimpleAudioInstruction(
-                    part.startCue,
-                    part.endCue,
-                    part.bpm,
-                    part.segment.start + part.beginAtCue,
-                    part.simpleSong.mediaFile.startingOffset,
-                    audioSource,
-                    partCompositionDiff);
-            audioInstruction.setInterpolatedRate(compositionPartDiff, compositionPartDiff);
-            instructions.append(audioInstruction);
+
+            Instruction instruction;
+            if(part instanceof LyricPart)
+                instruction = createLyricInstruction((LyricPart) part);
+            else
+                instruction = createAudioInstruction(part, partCompositionDiff, compositionPartDiff);
+            instructions.append(instruction);
         }
         return instructions;
     }
@@ -46,5 +45,28 @@ public class PlayStuff {
 
     public void stop() {
         renderer.stop();
+    }
+
+    private static AudioInstruction createAudioInstruction(Part part, Float partCompositionDiff, Float compositionPartDiff) throws Exception {
+        AudioSource audioSource = new MP3Source(new File(part.simpleSong.mediaFile.fileName));
+        AudioInstruction audioInstruction = new SimpleAudioInstruction(
+                part.startCue,
+                part.endCue,
+                part.bpm,
+                part.segment.start + part.beginAtCue,
+                part.simpleSong.mediaFile.startingOffset,
+                audioSource,
+                partCompositionDiff);
+        audioInstruction.setInterpolatedRate(compositionPartDiff, compositionPartDiff);
+        return audioInstruction;
+    }
+
+    private static Instruction createLyricInstruction(LyricPart lyricPart) {
+        return new SimpleLyricInstruction(
+                lyricPart.startCue,
+                lyricPart.endCue,
+                lyricPart.bpm,
+                lyricPart.text
+        );
     }
 }
