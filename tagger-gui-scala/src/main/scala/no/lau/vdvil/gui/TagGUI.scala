58fac03e6fb53a9a4dacb5e72bfbd4365c2feef1
Stig Lau <stiglau@McFeasty.local>
2010-09-20 14:59:17 +0200

    Updated Scala Tagger GUI, Now almost functional :)

--- tagger-gui-scala/src/main/scala/no/lau/vdvil/gui/TagGUI.scala
+++ tagger-gui-scala/src/main/scala/no/lau/vdvil/gui/TagGUI.scala
@@ -2,64 +2,97 @@ package no.lau.vdvil.gui
 
 import scala.swing._
 import event.ButtonClicked
-import no.lau.tagger.model.SimpleSong
 import no.bouvet.kpro.tagger.persistence.XStreamParser
 import java.io.File
-import no.bouvet.kpro.tagger.PlayerBase
+import no.lau.vdvil.cache.VdvilCacheHandler
+import no.lau.tagger.model.{Segment, SimpleSong}
+import java.util.ArrayList
+import collection.JavaConversions
+import scala.util.Random
 
 /**
  * Note - to make the TagGUI functional, it can be necessary to make a small change to the file and recompile.
  */
 object TagGUI extends SimpleSwingApplication {
   var dvlFilePath = System.getProperty("user.home") + "/kpro"
-  var playerBase = PlayerBase.NULL
+
+  var dvlTable: ScalaDynamicDvlTable = null
+  //These are just test variables
+  val returningDvlUrl = "http://kpro09.googlecode.com/svn/trunk/graph-gui-scala/src/main/resources/dvl/holden-nothing-93_returning_mix.dvl"
+  val returningDvlChecksum = "2f0bd28098bce29f555c713cc03ab625"
+
 
   def top = new MainFrame {
-    size = new Dimension(500, 300)
     title = "Tagging GUI"
+    //size = new Dimension(800, 600) //Need a frame repack
 
 
     contents = new BorderPanel {
-      val save = new Button("Save")
-      val load = new Button("Load File")
-
-      add(load, BorderPanel.Position.South)
-
-      listenTo(save)
-      listenTo(load)
-
-      reactions += {
-        case ButtonClicked(`save`) => {
-          println("Save button pressed")
-          def parser = new XStreamParser[SimpleSong]()
-          parser.save(playerBase.getSimpleSong(), dvlFilePath)
-          println("Saved: $dvlFilePath")
-          println(parser.toXml(playerBase.getSimpleSong))
-        }
-        case ButtonClicked(`load`) => {
-          val fileChooser = new FileChooser(new File(dvlFilePath))
-          val returnVal = fileChooser.showOpenDialog(this);
-          if (returnVal == FileChooser.Result.Approve) {
-            dvlFilePath = fileChooser.selectedFile.getAbsolutePath
-            add(new DynamicScalaTable(loadSongFromFile(dvlFilePath)), BorderPanel.Position.Center)
-            add(save, BorderPanel.Position.North)
-            //TODO Frame Repack
+      val save = new Button("Save") {
+        reactions += {
+          case ButtonClicked(_) => {
+            def parser = new XStreamParser[SimpleSong]()
+            parser.save(dvlTable.simpleSong, dvlFilePath)
+            println("Saved: $dvlFilePath")
+            println(parser.toXml(dvlTable.simpleSong))
           }
         }
       }
+      add(new Button("Load File") {
+        reactions += {
+          case ButtonClicked(_) => {
+            val fileChooser = new FileChooser(new File(dvlFilePath))
+            val returnVal = fileChooser.showOpenDialog(this);
+            if (returnVal == FileChooser.Result.Approve) {
+              dvlFilePath = fileChooser.selectedFile.getAbsolutePath
+
+              val simpleSong = NeatStuff.setAllNullIdsRandom(new VdvilCacheHandler().fetchSimpleSongAndCacheDvlAndMp3(returningDvlUrl, returningDvlChecksum))
+              dvlTable = new ScalaDynamicDvlTable(returningDvlUrl, simpleSong)
+
+              add(dvlTable.ui, BorderPanel.Position.Center)
+              add(save, BorderPanel.Position.North)
+              //TODO Frame Repack
+            }
+          }
+        }
+      }, BorderPanel.Position.South)
     }
   }
-
   def loadSongFromFile(dvlFile: String): SimpleSong = new XStreamParser().load(dvlFile)
 }
 
-object GUIStarter {
-  def main(args: Array[String]) = TagGUI.main(null)
-}
+object NeatStuff {
+  def setAllNullIdsRandom(original: SimpleSong): SimpleSong = {
+    val oldSegmentList = JavaConversions.asBuffer(original.segments)
+    var newSegmentList = new ArrayList[Segment]()
 
+    for (thisSegment <- oldSegmentList) {
+      if (thisSegment.id == null)
+        newSegmentList.add(cloneSegmentAndAddId(thisSegment))
+      else
+        newSegmentList.add(thisSegment)
+    }
+    return new SimpleSong(original.reference, original.mediaFile, newSegmentList, original.bpm)
+  }
 
-class DynamicScalaTable(song: SimpleSong) extends scala.swing.Component {
-  override lazy val peer = new PlayerBase(song).getDynamicTimeTable()
+  def cloneSegmentAndAddId(thisSegment: Segment): Segment = {
+    val id = String.valueOf(Math.abs(Random.nextLong))
+    return new Segment(id, thisSegment.start, thisSegment.end, thisSegment.text)
+  }
+
+  def updateSegmentInSimpleSong(editedSegment: Segment, editedSong: SimpleSong): SimpleSong = {
+    var newSegmentList = new ArrayList[Segment]()
+    val oldSegmentList = JavaConversions.asBuffer(editedSong.segments)
+
+    for (thisSegment <- oldSegmentList) {
+      if (thisSegment.id == editedSegment.id) {
+        newSegmentList.add(editedSegment)
+      } else {
+        newSegmentList.add(thisSegment)
+      }
+    }
+    return new SimpleSong(editedSong.reference, editedSong.mediaFile, newSegmentList, editedSong.bpm)
+  }
 }
 
 
