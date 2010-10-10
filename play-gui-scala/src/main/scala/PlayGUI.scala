16a41af14816ff03ea40a03f0b8c0971906b730c
Stig Lau <stiglau@mcfeasty.lan>
2010-10-10 12:42:18 +0200

    PlayGUI can now play simple segments

--- play-gui-scala/src/main/scala/PlayGUI.scala
+++ play-gui-scala/src/main/scala/PlayGUI.scala
@@ -1,8 +1,10 @@
 package vdvil
 
 import scala.swing._
-import no.lau.tagger.scala.model.ScalaSong
+import event.ButtonClicked
 import no.lau.vdvil.gui.TagGUI
+import no.lau.tagger.scala.model.{ScalaMediaFile, ScalaSong}
+import no.lau.vdvil.player.ScalaPlayer
 
 /**
  * Play GUI for playing .vdl files
@@ -12,19 +14,51 @@ object PlayGUI extends SimpleSwingApplication {
   val returningDvlUrl = "http://kpro09.googlecode.com/svn/trunk/graph-gui-scala/src/main/resources/dvl/holden-nothing-93_returning_mix.dvl"
   val testSong: ScalaSong = TagGUI.fetchDvlAndMp3FromWeb(returningDvlUrl).get
 
-  val bpmField = new TextField(testSong.bpm.toString, 3) {
-    reactions += {case _ => if (!text.isEmpty) {testSong.bpm = text.toFloat; println("BPM Changed to " + testSong.bpm)}}
-  }
-
 
   def top = new MainFrame {
     title = "Play GUI"
-    size = new Dimension(800, 600) //Need a frame repack
-
     contents = new FlowPanel {
-      contents += new Button("Play")
-      contents += new Button("Pause")
+      contents += new Label("Start from")
+      contents += startField
+      contents += new Label("Stop")
+      contents += stopField
+      contents += playButton
+      contents += new Label("BPM not implemented")
       contents += bpmField
     }
   }
+
+  val bpmField = new TextField(testSong.bpm.toString, 3) {
+    reactions += {case _ => if (!text.isEmpty) {testSong.bpm = text.toFloat; println("BPM Changed to " + testSong.bpm)}}
+  }
+  val startField = new TextField("0.0", 4) 
+  val stopField = new TextField("64.0", 4)
+  val playButton = new Button("Play") {
+    reactions += {case ButtonClicked(_) => playSegment(startField.text.toFloat, stopField.text.toFloat, testSong)}
+  }
+
+
+  var songPlayer: ScalaPlayer = null
+
+  /**
+   * Plays the segment of your choice
+   */
+  def playSegment(startFrom: Float, stopAt:Float, song: ScalaSong) {
+    if (songPlayer != null)
+      songPlayer.playPause(-1F, -1F) //Call to stop the player
+    val mf = song.mediaFile
+    val pathToMp3Option = TagGUI.cacheHandler.retrievePathToFileFromCache(mf.fileName, mf.checksum)
+    if (pathToMp3Option.isDefined) {
+      val copyOfSong = new ScalaSong(song.reference, new ScalaMediaFile(pathToMp3Option.get, mf.checksum, mf.startingOffset), song.segments, song.bpm)
+      songPlayer = new ScalaPlayer(copyOfSong)
+      songPlayer.playPause(startFrom, stopAt)
+    }
+  }
+
+  /*
+ override def startup(args: Array[String]) {
+   val t = top
+   t.size_=(new Dimension(800, 600))
+   t.visible = true
+ } */
 }
