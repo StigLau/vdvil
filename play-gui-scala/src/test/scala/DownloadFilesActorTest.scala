586cb740f53f94fc3d1ea8a98727912bdf573cdf
Stig Lau <stiglau@McFeasty.local>
2010-10-18 07:40:42 +0200

    All we need now is a GUI

--- play-gui-scala/src/test/scala/DownloadFilesActorTest.scala
+++ play-gui-scala/src/test/scala/DownloadFilesActorTest.scala
@@ -13,11 +13,14 @@ class DownloadFilesActorTest {
             Nil
 
 
-    val coordinator = new DownloadCoordinatorActor
+    val coordinator = new DownloadCoordinatorActor(urls.size)
     coordinator.start
 
     val baseUrl = "http://kpro09.googlecode.com/svn/trunk/graph-gui-scala/src/main/resources/dvl/"
-    urls.foreach(url => new DownloadActor(baseUrl + url, coordinator).start)
+    urls.foreach {
+      url => new DownloadActor(baseUrl + url, coordinator).start
+      Thread.sleep(100)
+    }
   }
 
 
@@ -28,33 +31,47 @@ class DownloadActor(url: String, coordinator: Actor) extends Actor {
 
   def act() {
     coordinator ! DownloadingDvl(Dvl(url))
-    val unconvertedSong:ScalaSong = cacheHandler.fetchSimpleSongAndCacheDvlAndMp3(url, null)
+    val unconvertedSong: ScalaSong = cacheHandler.fetchSimpleSongAndCacheDvlAndMp3(url, null)
     coordinator ! ConvertingAndAddingMissingIds(Dvl(url))
     val song = no.lau.vdvil.gui.NeatStuff.convertAllNullIDsToRandom(unconvertedSong)
     val mf = song.mediaFile
-    val pathToMp3Option: Option[String] = cacheHandler.retrievePathToFileFromCache(mf.fileName, mf.checksum)
-    val finSong = new ScalaSong(song.reference, new ScalaMediaFile(pathToMp3Option.get, mf.checksum, mf.startingOffset), song.segments, song.bpm)
-    coordinator ! FinishedDownloading(finSong)
+    cacheHandler.retrievePathToFileFromCache(mf.fileName, mf.checksum).foreach {
+      pathToMp3Option => coordinator ! FinishedDownloading(new ScalaSong(song.reference, new ScalaMediaFile(pathToMp3Option, mf.checksum, mf.startingOffset), song.segments, song.bpm))
+    }
   }
 }
 
-class DownloadCoordinatorActor extends Actor {
+class DownloadCoordinatorActor(numberOfFilesToDownload: Int) extends Actor {
+  import scala.collection.mutable.Set
+  val songSet = Set[ScalaSong]()
+
   def act() {
     loop {
       react {
         case downloading: DownloadingDvl => println("Downloading: " + downloading.dvl.url)
         case converting: ConvertingAndAddingMissingIds => println("Converting: " + converting.dvl.url)
-        case finished: FinishedDownloading => println("Finished downloading: " + finished.song.mediaFile.fileName)
-        case error: ErrorDownloading => println("Error downloading: " + error.message)
+        case finished: FinishedDownloading => {
+          println("Finished downloading: " + finished.song.mediaFile.fileName)
+          songSet += finished.song
+          if (songSet.size >= numberOfFilesToDownload) {
+            println("Stopping Coordinator")
+            exit()
+          }
+        }
+        case error: ErrorDownloading => {
+          println("Error downloading: " + error.message)
+          println("Will now stop downloading procedure")
+          exit()
+        }
       }
     }
   }
 }
-case class Dvl(url:String)
-case class DownloadingDvl(dvl:Dvl)
-case class ConvertingAndAddingMissingIds(dvl:Dvl)
-case class FinishedDownloading(song:ScalaSong)
-case class ErrorDownloading(message:String)
+case class Dvl(url: String)
+case class DownloadingDvl(dvl: Dvl)
+case class ConvertingAndAddingMissingIds(dvl: Dvl)
+case class FinishedDownloading(song: ScalaSong)
+case class ErrorDownloading(message: String)
 
 
 
