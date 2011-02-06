47d51a139281dec7b0216a232cdc12eaa2836cba
Stig Lau <stiglau@mcfeasty.lan>
2011-02-06 22:19:01 +0100

    PlayingAMix is now a test which plays... a mix

--- tagger-gui/src/test/java/no/lau/vdvil/PlayingAMix.java
+++ tagger-gui/src/test/java/no/lau/vdvil/PlayingAMix.java
@@ -4,6 +4,7 @@ import com.thoughtworks.xstream.XStream;
 import no.bouvet.kpro.tagger.PlayStuff;
 import no.lau.tagger.model.*;
 import org.codehaus.httpcache4j.cache.VdvilCacheStuff;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,29 +14,20 @@ import java.util.List;
 
 public class PlayingAMix {
 
-    SimpleSong nothing;
-    SimpleSong psylteFlesk;
-
-    VdvilCacheStuff persistantCache = new VdvilCacheStuff(new File("/tmp/vdvil"));
+    VdvilCacheStuff cache = new VdvilCacheStuff(new File("/tmp/vdvil"));
     String holdenUrl = "http://kpro09.googlecode.com/svn/trunk/graph-gui-scala/src/main/resources/dvl/holden-nothing-93_returning_mix.dvl";
     String psylteUrl = "http://github.com/StigLau/vdvil/raw/master/tagger-gui/src/test/resources/loaderror-psylteflesk.dvl";
 
-    public static void main(String[] args) {
-        PlayingAMix test = new PlayingAMix();
-        test.beforeMethod();
-        try {
-            test.testPlayingSomeStuff();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void testMixing() throws FileNotFoundException, InterruptedException {
+        SimpleSong nothing = createSimpleSongFromXML(cache.fetchAsStream(holdenUrl));
+        SimpleSong psylteFlesk = createSimpleSongFromXML(cache.fetchAsStream(psylteUrl));
+        List<AudioPart> parts = testPlayingSomeStuff(nothing, psylteFlesk);
+        PlayStuff playStuff = new PlayStuff(new Composition(130F, parts), cache);
+        playStuff.play(4F);
+        Thread.sleep(2000);
     }
 
-    public void beforeMethod() {
-        InputStream holdenStream = persistantCache.fetchAsInputStream(holdenUrl);
-        nothing = createSimpleSongFromXML(holdenStream);
-        InputStream psylteFleskStream = persistantCache.fetchAsInputStream(psylteUrl);
-        psylteFlesk = createSimpleSongFromXML(psylteFleskStream);
-    }
 
     private SimpleSong createSimpleSongFromXML(InputStream inputStream) {
         XStream xstream = new XStream();
@@ -44,16 +36,13 @@ public class PlayingAMix {
         return (SimpleSong) xstream.fromXML(inputStream);
     }
 
-    public void testPlayingSomeStuff() throws Exception {
+    public List<AudioPart> testPlayingSomeStuff(SimpleSong nothing, SimpleSong psylteFlesk) {
         List<AudioPart> parts = new ArrayList<AudioPart>();
-        System.out.println("playing mix");
         parts.add(new AudioPart(nothing, 0F, 20F, nothing.segments.get(3)));
         parts.add(new AudioPart(psylteFlesk, 8F, 16F, psylteFlesk.segments.get(4)));
         parts.add(new AudioPart(nothing, 12F, 52F, nothing.segments.get(6)));
         parts.add(new AudioPart(nothing, 19.99F, 32F, nothing.segments.get(3)));
-
-        PlayStuff playStuff = new PlayStuff(new Composition(130F, parts), persistantCache);
-        playStuff.play(0F);
+        return parts;
     }
 }
 
