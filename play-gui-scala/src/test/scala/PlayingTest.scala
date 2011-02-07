4752cad18bf8fb2e67255333b2861ec9d80da749
Stig Lau <stiglau@McFeasty.local>
2011-02-07 09:01:45 +0100

    Not using float for starting points

--- play-gui-scala/src/test/scala/PlayingTest.scala
+++ play-gui-scala/src/test/scala/PlayingTest.scala
@@ -2,12 +2,13 @@ package no.lau.vdvil.player.test
 
 import org.junit.Test
 import no.lau.vdvil.player. {JavaZoneDemoComposition, ScalaCompositionPlayer}
+import no.lau.vdvil.cache.ScalaCacheHandler
 
 class PlayingTest {
   @Test def playMix {
     val composition = JavaZoneDemoComposition.masterMix.asComposition
     val player = new ScalaCompositionPlayer(Some(composition))
-    player.play(0F, 150F)
+    player.play(0, 150F)
     Thread.sleep((16*60/150)*1000)
     player.stop
   }
