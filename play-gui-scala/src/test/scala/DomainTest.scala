503ea36eb940f2864e8581411feb32503c64021d
Stig Lau <stiglau@McFeasty.local>
2011-02-07 09:01:26 +0100

    commented out unwritten test

--- play-gui-scala/src/test/scala/DomainTest.scala
+++ play-gui-scala/src/test/scala/DomainTest.scala
@@ -1,7 +1,7 @@
 package no.lau.vdvil.domain.player
 
-import org.junit.Test
 import org.junit.Assert._
+import org.junit.{Ignore, Test}
 
 
 class DomainTest {
@@ -13,5 +13,10 @@ class DomainTest {
   def testData:MasterMix = {
     MasterMix("name", 140F,
       MasterPart(Dvl("url", "name"),0, 16, "0") ::MasterPart(Dvl("url", "name1"),32, 64, "0") ::MasterPart(Dvl("url", "name"),16, 32, "0") :: Nil)
+  }
+
+  @Ignore
+  @Test def xmlParsing {
+    fail("Need to write this test!! Load xml example files")
   }
 }
