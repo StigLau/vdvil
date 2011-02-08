81e9b3e7869b6960b057b87363f51ed0c7ba163d
Stig Lau <stiglau@McFeasty.local>
2011-02-08 12:19:04 +0100

    forgot to add this file

---
+++ tagger-gui-scala/src/main/scala/ScalaSongXmlPersister.scala
@@ -0,0 +1,16 @@
+package no.lau.vdvil.gui
+
+
+import no.bouvet.kpro.tagger.persistence.XStreamParser
+import no.lau.tagger.model.SimpleSong
+import org.slf4j.LoggerFactory
+import no.lau.tagger.scala.model._
+
+object ScalaSongXmlPersister {
+  val log = LoggerFactory.getLogger(ScalaSongXmlPersister.getClass)
+  var parser = new XStreamParser[SimpleSong]
+
+  def printableXml(scalaSong: ScalaSong): String = parser.toXml(TranslateTo.from(scalaSong))
+
+  def save(song: ScalaSong, path: String): Unit = parser.save(TranslateTo.from(song), path)
+  }
