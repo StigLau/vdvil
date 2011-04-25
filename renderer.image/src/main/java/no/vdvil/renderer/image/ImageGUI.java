f2206f9c2532c483063c723f47c18429856eb8ee
Stig Lau <stig@lau.no>
2011-04-25 11:52:36 +0200

    Updated image rendering API to a little simpler version

--- renderer.image/src/main/java/no/vdvil/renderer/image/ImageGUI.java
+++ renderer.image/src/main/java/no/vdvil/renderer/image/ImageGUI.java
@@ -1,14 +1,10 @@
 package no.vdvil.renderer.image;
 
-import no.vdvil.renderer.image.ImageListener;
 import javax.swing.*;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ImageGUI {
     JFrame frame;
-    public final ImagePanel imagePanel = new ImagePanel();
+    final ImagePanel imagePanel = new ImagePanel();
 
     public ImageGUI(int width, int height) {
         frame = new JFrame("ImageRendererGUITest");
@@ -16,5 +12,9 @@ public class ImageGUI {
         frame.setSize(width, height);
         frame.getContentPane().add(imagePanel);
         frame.setVisible(true);
+    }
+
+    public ImageListener getImageListener() {
+        return imagePanel;
     }
 }
--- renderer.image/src/test/java/no/lau/vdvil/renderer/image/ImageRenderingTest.java
+++ renderer.image/src/test/java/no/lau/vdvil/renderer/image/ImageRenderingTest.java
@@ -1,23 +1,18 @@
 package no.lau.vdvil.renderer.image;
 
 import no.vdvil.renderer.image.ImageGUI;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Before;
+import no.vdvil.renderer.image.ImageListener;
 import org.junit.Test;
-
 import java.net.MalformedURLException;
-import java.net.URL;
 
 public class ImageRenderingTest {
     @Test
     public void testRenderingImage() throws InterruptedException, MalformedURLException {
-        ImageGUI imageGUI = new ImageGUI(800, 600);
+        ImageListener listener = new ImageGUI(800, 600).getImageListener();
         Thread.sleep(100);
-        imageGUI.imagePanel.show(ClassLoader.getSystemResource("pink_teddy.jpg"));
+        listener.show(ClassLoader.getSystemResource("pink_teddy.jpg"));
         Thread.sleep(200);
-        imageGUI.imagePanel.show(ClassLoader.getSystemResource("dj-teddy.jpg"));
+        listener.show(ClassLoader.getSystemResource("dj-teddy.jpg"));
         Thread.sleep(200);
     }
 }
