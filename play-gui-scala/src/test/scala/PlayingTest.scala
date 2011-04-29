96bff42e5415c138c883351d089b41377a2d0155
Stig Lau <stig@lau.no>
2011-04-29 08:38:21 +0200

    Testing out with different solutions

--- play-gui-scala/src/test/scala/PlayingTest.scala
+++ play-gui-scala/src/test/scala/PlayingTest.scala
@@ -2,9 +2,17 @@ package no.lau.vdvil.player.test
 
 import org.junit.Test
 import no.lau.vdvil.player.{ScalaCompositionPlayer, JavaZoneDemoComposition}
-import no.lau.vdvil.domain.player.ScalaComposition
+import org.codehaus.httpcache4j.cache.VdvilHttpCache
+import no.lau.vdvil.domain.player.{ImagePart, ScalaComposition}
 
 class PlayingTest {
+  /*
+  @Test
+  def cacheFiles{
+    val imagePart:List[ImagePart] = JavaZoneDemoComposition.masterMix.collect{case x:ImagePart => x}
+    val cache = new VdvilHttpCache
+    imagePart.foreach(cache.fetchFromInternetOrRepository(_.imageUrl))
+  } */
   //TODO Why does it download so many files!?!
   @Test def playMix {
     val composition:ScalaComposition = JavaZoneDemoComposition.masterMix.asComposition
