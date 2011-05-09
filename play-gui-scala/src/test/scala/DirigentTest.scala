ad204a1c443811b067fe735d7f871f7d41a5d788
Stig Lau <stig@lau.no>
2011-05-09 22:24:17 +0200

    Added the Dirigent, don't think it works quite yet :)

--- play-gui-scala/src/test/scala/DirigentTest.scala
+++ play-gui-scala/src/test/scala/DirigentTest.scala
@@ -1,14 +1,8 @@
 package no.lau.vdvil.dirigent
 
-import no.lau.vdvil.cache.VdvilCache
-import no.lau.vdvil.domain.player.{ScalaComposition, MasterMix}
 import org.codehaus.httpcache4j.cache.VdvilHttpCache
 import org.junit.Test
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import java.io.InputStream
-import xml.XML
-import no.lau.vdvil.persistence.MasterMixXML
+import java.net.URL
 
 /**
  * A Simple structuring of the Dirigent
@@ -20,64 +14,12 @@ class DirigentTest {
 
   @Test def testSetupAndPlaying {
     val player = new no.lau.vdvil.player.ScalaCompositionPlayer(None)
-    val dirigent = new Dirigent(VdvilHttpCache.create :: Nil, new CompositionHandler :: Nil, player) {
+    val dirigent = new Dirigent(ScalaHttpCache :: LocalFileCache :: Nil, new CompositionHandler :: Nil, player) {
       playbackBpm = 150F
-      prepare(compositionUrl)
+      prepare(new URL(compositionUrl))
     }
     dirigent.play(32)
     Thread.sleep(5000)
     dirigent.stop
-  }
-}
-
-class Dirigent(downloaders: List[VdvilCache], handlers:List[VdvilHandler], var player:no.lau.vdvil.player.ScalaCompositionPlayer) {
-  val log: Logger = LoggerFactory.getLogger(classOf[Dirigent])
-  var playbackBpm: Float = 0F
-
-  def prepare(compositionUrl: String) {
-    for (downloader <- downloaders) { //TODO Perhaps use for-comprehension for matching correct downloader, and avoiding nagging error logging!
-      if (downloader.accepts(compositionUrl)) {
-        log.info("Trying to download {} with ", compositionUrl, downloader)
-        var inputStream: InputStream = downloader.fetchAsStream(compositionUrl)
-        var mimeType: String = downloader.mimeType(compositionUrl)
-        mimeType = "composition/xml" //TODO Set the mimetype staticly until server is finished.
-        for (handler <- handlers) {
-          if (handler.accepts(mimeType)) {
-            player.scalaCompositionOption = handler.load(inputStream, mimeType)
-          }
-        }
-      }
-      else log.warn("Downloader {} did not accept {}", downloader, compositionUrl)
-    }
-  }
-
-
-  def play(startAtCue: Int) {
-    log.info("Play ")
-    player.play(startAtCue, playbackBpm)
-  }
-
-  def stop {
-    log.info("Stop ")
-    player.stop
-  }
-}
-
-trait VdvilHandler {
-  def accepts(mimeType: String): Boolean
-
-  def load(inputStream: InputStream, mimeType: String): Option[ScalaComposition]
-}
-
-class CompositionHandler extends VdvilHandler {
-  val log: Logger = LoggerFactory.getLogger(classOf[VdvilHandler])
-
-  def accepts(mimeType: String): Boolean = "composition/xml".equals(mimeType)
-
-  def load(inputStream: InputStream, mimeType: String): Option[ScalaComposition] = {
-    if (accepts(mimeType))
-      Some(MasterMixXML.fromXML(XML.load(inputStream)).asComposition)
-    else
-      None
   }
 }
