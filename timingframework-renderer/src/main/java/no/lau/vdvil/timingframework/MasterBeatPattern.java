33da73e69429d30d995b89d4fdfe5274a50e5c82
Stig Lau <stig@lau.no>
2011-06-08 22:32:21 +0200

    Added some clear voyance

--- timingframework-renderer/src/main/java/no/lau/vdvil/timingframework/MasterBeatPattern.java
+++ timingframework-renderer/src/main/java/no/lau/vdvil/timingframework/MasterBeatPattern.java
@@ -1,9 +1,23 @@
 package no.lau.vdvil.timingframework;
 
+/**
+ * This is the ground basis for most of the calculations concerning timing
+ * @author Stig@lau.no
+ * @since June 2011
+ */
 public class MasterBeatPattern {
     public final Integer fromBeat;
     public final Integer toBeat;
     final Float masterBpm;
+    /**
+     * Indicating that the most fine-grained timing can be performed at this level.
+     * In time will be set higher - 44100 or somehting!
+     */
+    public static final int FRAMERATE_PER_SECOND = 1000;
+    /**
+     * DUH! But OK to know what these magic numbers are ;)
+     */
+    public static final int SECONDS_IN_A_MINUTE = 60;
 
     public MasterBeatPattern(int fromBeat, int toBeat, Float masterBpm) {
         this.fromBeat = fromBeat;
@@ -11,14 +25,18 @@ public class MasterBeatPattern {
         this.masterBpm = masterBpm;
     }
 
-
-    public Float getBpm(int beat) {
+    /**
+     * For calulating what the BPM is at a specific
+     * @param beat teh beat
+     * @return Beats Per Minute
+     */
+    public Float bpmAt(int beat) {
         return masterBpm;
     }
 
     public Float durationCalculation() {
         int durationBeats = toBeat - fromBeat;
-        return durationBeats * 60 * 1000 / masterBpm;
+        return durationBeats * SECONDS_IN_A_MINUTE * FRAMERATE_PER_SECOND / masterBpm;
     }
 
     public MasterBeatPattern duration(int fromBeat, int toBeat) {
