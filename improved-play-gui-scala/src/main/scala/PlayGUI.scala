ee1e3fd53409b729c53ddb7a56518f94ff07315e
Stig Lau <stig@lau.no>
2011-06-21 22:58:34 +0200

    It Compiles - It Compiles!!!

--- improved-play-gui-scala/src/main/scala/PlayGUI.scala
+++ improved-play-gui-scala/src/main/scala/PlayGUI.scala
@@ -2,7 +2,6 @@ package no.lau.vdvil.player
 
 import scala.swing._
 import event.ButtonClicked
-import no.lau.vdvil.downloading._
 import swing.TabbedPane.Page
 import org.slf4j.LoggerFactory
 import java.net.URL
@@ -24,7 +23,7 @@ object PlayGUI extends SimpleSwingApplication {
     menuBar = new MenuBar {
       contents += new Menu("Load") {
         contents += new MenuItem(Action("from web") {
-          Dialog.showInput(menuBar, "", "Load from", Dialog.Message.Plain, Swing.EmptyIcon, Nil, javaZoneDemoCompositionUrl.toString).map(chosenPath => startDownload(new URL(chosenPath)))
+          //Dialog.showInput(menuBar, "", "Load from", Dialog.Message.Plain, Swing.EmptyIcon, Nil, javaZoneDemoCompositionUrl.toString).map(chosenPath => startDownload(new URL(chosenPath)))
         })
       }
     }
@@ -41,7 +40,7 @@ object PlayGUI extends SimpleSwingApplication {
  }
 }
 
-class PlayPanel(val masterMix: Composition) {
+class PlayPanel(val composition: no.lau.vdvil.handler.Composition) {
   lazy val ui = new FlowPanel {
     contents += new Label("Start from")
     contents += startField
@@ -51,19 +50,19 @@ class PlayPanel(val masterMix: Composition) {
     contents += new Label("BPM")
     contents += bpmField
   }
-
-  val bpmField = new TextField(masterMix.masterBpm.toString, 4)
+  val beatPattern = composition.masterBeatPattern
+  val bpmField = new TextField(beatPattern.masterBpm.toString, 4)
   val startField = new TextField("0", 4)
-  val stopField = new TextField(masterMix.durationAsBeats.toString, 4)
+  val stopField = new TextField(beatPattern.toBeat.toString, 4)
   val playCompositionButton = new Button("Play Composition") {
     reactions += {case ButtonClicked(_) => compositionPlayer.pauseAndplay(startField.text.toInt)}
   }
   val compositionPlayer = new PreconfiguredVdvilPlayer() {
     def pauseAndplay(startFrom: Int) {
       stop
-      masterMix.masterBpm = bpmField.text.toFloat
-      scalaCompositionOption = Some(masterMix.asComposition)
-      play(startFrom, masterMix.masterBpm)
+      //masterMix.masterBpm = bpmField.text.toFloat
+      //scalaCompositionOption = Some(masterMix.asComposition)
+      play(startFrom)
     }
   }
 }
