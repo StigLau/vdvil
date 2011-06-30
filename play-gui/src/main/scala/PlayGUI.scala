bde7f02aa6b40e96f7e496b560b0217c99e154f5
Stig Lau <stig@lau.no>
2011-06-30 19:33:32 +0200

    Extracted PlayPanel from PlayGUI

--- play-gui/src/main/scala/PlayGUI.scala
+++ play-gui/src/main/scala/PlayGUI.scala
@@ -1,15 +1,9 @@
 package no.lau.vdvil.player
 
 import scala.swing._
-import event.ButtonClicked
 import swing.TabbedPane.Page
 import org.slf4j.LoggerFactory
 import java.net.URL
-import no.vdvil.renderer.audio.TestMp3s
-import no.lau.vdvil.playback.PreconfiguredVdvilPlayer
-import no.lau.vdvil.handler._
-import no.lau.vdvil.handler.persistence._
-import no.lau.vdvil.timing.MasterBeatPattern
 
 /**
  * Play GUI for playing .vdl files
@@ -48,46 +42,4 @@ object PlayGUI extends SimpleSwingApplication {
     val playPanel = new PlayPanel(url)
     tabs.pages.append(new Page(playPanel.name, playPanel.ui))
   }
-}
-
-class PlayPanel(val url:URL) {
-  val cache = PreconfiguredVdvilPlayer.downloadAndParseFacade
-  var composition: Composition = fetchComposition(url)
-  def name = composition.name
-
-  lazy val ui = new FlowPanel {
-    contents += new Label("Start from")
-    contents += startField
-    contents += new Label("Stop")
-    contents += stopField
-    contents += new Label("BPM")
-    contents += bpmField
-    contents += playCompositionButton
-    contents += reloadButton
-  }
-  val beatPattern = composition.masterBeatPattern
-  val bpmField = new TextField(beatPattern.masterBpm.toString, 4)
-  val startField = new TextField("0", 4)
-  val stopField = new TextField(beatPattern.toBeat.toString, 4)
-  val playCompositionButton = new Button("Play Composition") {
-    reactions += {case ButtonClicked(_) => compositionPlayer.pauseAndplay(new MasterBeatPattern(startField.text.toInt, stopField.text.toInt, bpmField.text.toFloat))}
-  }
-  val reloadButton = new Button("Reload") {
-    reactions += {case ButtonClicked(_) => {
-      cache.setRefreshCaches(true)
-      composition = fetchComposition(url)
-      cache.setRefreshCaches(false)
-      stopField.text_=(composition.masterBeatPattern.toBeat.toString)
-    }}
-  }
-  val compositionPlayer = new PreconfiguredVdvilPlayer() {
-    def pauseAndplay(beatPattern:MasterBeatPattern) {
-      stop
-      init(composition, beatPattern)
-      println("Playing " + composition.name + " " + beatPattern)
-      play
-    }
-  }
-
-  def fetchComposition(url:URL) = cache.parse(PartXML.create(url)).asInstanceOf[Composition]
 }
---
+++ play-gui/src/main/scala/PlayPanel.scala
@@ -0,0 +1,51 @@
+package no.lau.vdvil.player
+
+import java.net.URL
+import no.lau.vdvil.handler.Composition
+import no.lau.vdvil.handler.persistence.PartXML
+import no.lau.vdvil.playback.PreconfiguredVdvilPlayer
+import no.lau.vdvil.timing.MasterBeatPattern
+import swing._
+import event.ButtonClicked
+
+class PlayPanel(val url:URL) {
+  val cache = PreconfiguredVdvilPlayer.downloadAndParseFacade
+  var composition: Composition = fetchComposition(url)
+  def name = composition.name
+
+  lazy val ui = new FlowPanel {
+    contents += new Label("Start from")
+    contents += startField
+    contents += new Label("Stop")
+    contents += stopField
+    contents += new Label("BPM")
+    contents += bpmField
+    contents += playCompositionButton
+    contents += reloadButton
+  }
+  val beatPattern = composition.masterBeatPattern
+  val bpmField = new TextField(beatPattern.masterBpm.toString, 4)
+  val startField = new TextField("0", 4)
+  val stopField = new TextField(beatPattern.toBeat.toString, 4)
+  val playCompositionButton = new Button("Play Composition") {
+    reactions += {case ButtonClicked(_) => compositionPlayer.pauseAndplay(new MasterBeatPattern(startField.text.toInt, stopField.text.toInt, bpmField.text.toFloat))}
+  }
+  val reloadButton = new Button("Reload") {
+    reactions += {case ButtonClicked(_) => {
+      cache.setRefreshCaches(true)
+      composition = fetchComposition(url)
+      cache.setRefreshCaches(false)
+      stopField.text_=(composition.masterBeatPattern.toBeat.toString)
+    }}
+  }
+  val compositionPlayer = new PreconfiguredVdvilPlayer() {
+    def pauseAndplay(beatPattern:MasterBeatPattern) {
+      stop
+      init(composition, beatPattern)
+      println("Playing " + composition.name + " " + beatPattern)
+      play
+    }
+  }
+
+  def fetchComposition(url:URL) = cache.parse(PartXML.create(url)).asInstanceOf[Composition]
+}
