732dfc93bb6127b6e41ea70ffee5e2914b57919c
Stig Lau <stiglau@McFeasty.local>
2011-09-27 19:01:54 +0200

    TestFile works

--- simple-cache/src/test/java/no/lau/vdvil/cache/SimpleCacheTest.java
+++ simple-cache/src/test/java/no/lau/vdvil/cache/SimpleCacheTest.java
@@ -6,52 +6,48 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SimpleCacheTest {
 
-    URL returningMp3;
+    URL localTestFile;
     String returningChecksum = "e6746326540c2f847b054bb329a5d54a";
     URL testFile;
     URL ftpFile;
 
     SimpleCacheImpl cacheImpl = new SimpleCacheImpl();
-    private URL asFile;
 
     @Before
     public void setup() throws MalformedURLException {
-        returningMp3 = new URL("HTTP://kpro09.googlecode.com/svn/test-files/holden-nothing-93_returning_mix.mp3");
-        asFile = new URL("file://localhost/tmp/holden-nothing-93_returning_mix.mp3");
+        localTestFile = getClass().getResource("/empty_testfile.txt");
         testFile = new URL("file://tmp/vdvil");
         ftpFile = new URL("ftp://not.supported");
     }
 
     @Test
     public void acceptsTest() {
-        assertTrue(cacheImpl.accepts(returningMp3));
+        assertTrue(cacheImpl.accepts(localTestFile));
         assertTrue(cacheImpl.accepts(testFile));
         assertFalse(cacheImpl.accepts(ftpFile));
     }
 
     @Test
     public void cleanCacheTest() throws IOException {
-        assertTrue(cacheImpl.removeFromCache(asFile));
-        cacheImpl.fetchFromInternetOrRepository(asFile, returningChecksum);
+        assertTrue(cacheImpl.removeFromCache(localTestFile));
+        cacheImpl.fetchFromInternetOrRepository(localTestFile, returningChecksum);
     }
 
     @Test
     public void cacheTest() throws IOException {
-        cacheImpl.fetchFromInternetOrRepository(asFile, returningChecksum);
+        cacheImpl.fetchFromInternetOrRepository(localTestFile, returningChecksum);
     }
 
     @Test
     public void refreshingCacheWorks() throws IOException {
         cacheImpl.setRefreshCache(true);
-        cacheImpl.fetchFromInternetOrRepository(asFile, returningChecksum);
+        cacheImpl.fetchFromInternetOrRepository(localTestFile, returningChecksum);
         cacheImpl.setRefreshCache(false);
-        cacheImpl.fetchFromInternetOrRepository(asFile, returningChecksum);
+        cacheImpl.fetchFromInternetOrRepository(localTestFile, returningChecksum);
     }
 }
