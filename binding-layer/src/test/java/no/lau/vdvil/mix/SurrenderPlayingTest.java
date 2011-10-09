d4426fc53eeebec9736305b37e2e99e7877e741d
Stig Lau <stig@lau.no>
2011-10-09 20:49:58 +0200

    Tweaked a little on the test

--- binding-layer/src/test/java/no/lau/vdvil/mix/SurrenderPlayingTest.java
+++ binding-layer/src/test/java/no/lau/vdvil/mix/SurrenderPlayingTest.java
@@ -16,26 +16,15 @@ public class SurrenderPlayingTest extends SuperPlayingSetup {
     @Test
     public void play() throws IOException {
         surrenderDvl = ClassLoader.getSystemResource("Way_Out_West-Surrender-Eelke_Kleijn_Remix.dvl.xml");
-        super.play(new MasterBeatPattern(0, 24, 150F)); //(0, 308, 125)
+        super.play(new MasterBeatPattern(0, 32, 150F)); //(0, 308, 125)
     }
 
     @Override
     protected Composition compose(MasterBeatPattern masterBeatPattern) throws IOException {
         List<MultimediaPart> parts = new ArrayList<MultimediaPart>();
-        /*
-        parts.add(createAudioPart("0-128Surrender", 0, 128, surrenderDvl, downloader)); // Intro -- Buildup
-        parts.add(createAudioPart("128-256Surrender", 128, 256, surrenderDvl, downloader)); // AAaaaaaah -- Start -- Flurry  <-- Can be cut up!
-        parts.add(createAudioPart("256-352Surrender", 256, 352, surrenderDvl, downloader)); // Elguitar
-        parts.add(createAudioPart("352-512Surrender", 352, 480, surrenderDvl, downloader)); // Just surrender x2
-        parts.add(createAudioPart("480-544Surrender", 480, 544, surrenderDvl, downloader)); // Synth X2
-        parts.add(createAudioPart("544-656Surrender", 544, 656, surrenderDvl, downloader)); // Soft song duette + flurry --
-        parts.add(createAudioPart("656-768Surrender", 656, 768, surrenderDvl, downloader)); // Flurry butterflies
-        parts.add(createAudioPart("768-912Surrender", 768, 912, surrenderDvl, downloader));
-        */
-
         parts.add(createImagePart("Diving Teddy", 0, 4, new URL("http://www.shinyshiny.tv/teddy%20bear%201.jpg")));
         parts.add(createImagePart("Dead Teddy", 16, 20, new URL("http://fc03.deviantart.net/fs17/f/2007/182/f/4/Dead_Teddy_by_Cast_Down_Doll.jpg")));
-        parts.add(createImagePart("Diving Teddy", 80, 84, new URL("http://www.shinyshiny.tv/teddy%20bear%201.jpg")));
+        parts.add(createImagePart("Diving Teddy", 80, 84, new URL("http://farm3.static.flickr.com/2095/2282261838_276a37d325_o_d.jpg")));
         parts.add(createImagePart("Dead Teddy", 144, 148, new URL("http://fc03.deviantart.net/fs17/f/2007/182/f/4/Dead_Teddy_by_Cast_Down_Doll.jpg")));
         parts.add(createImagePart("Diving Teddy", 240, 244, new URL("http://www.shinyshiny.tv/teddy%20bear%201.jpg")));
 
@@ -44,6 +33,7 @@ public class SurrenderPlayingTest extends SuperPlayingSetup {
         parts.add(createAudioPart("352-512Surrender", 80, 144, surrenderDvl, downloader));
         parts.add(createAudioPart("352-512Surrender", 144, 240, surrenderDvl, downloader));
         parts.add(createAudioPart("480-544Surrender", 240, 304, surrenderDvl, downloader)); // Synth X2
+        parts.add(createAudioPart("768-896Surrender", 304, 432, surrenderDvl, downloader));
 
         return new Composition("SurrenderTest", masterBeatPattern, parts, TestMp3s.NULL);
     }
