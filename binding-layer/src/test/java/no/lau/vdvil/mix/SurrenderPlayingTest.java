948546fdc825d7909bc80b7dad8878bf619fc372
Stig Lau <stig@lau.no>
2011-10-17 14:40:07 +0200

    Pictures play a little longer

--- binding-layer/src/test/java/no/lau/vdvil/mix/SurrenderPlayingTest.java
+++ binding-layer/src/test/java/no/lau/vdvil/mix/SurrenderPlayingTest.java
@@ -17,16 +17,16 @@ public class SurrenderPlayingTest extends SuperPlayingSetup {
     @Test
     public void play() throws IOException {
         surrenderDvl = ClassLoader.getSystemResource("Way_Out_West-Surrender-Eelke_Kleijn_Remix.dvl.xml");
-        super.play(new MasterBeatPattern(0, 32, 150F));
+        super.play(new MasterBeatPattern(0, 16, 150F));
     }
 
     @Override
     protected Composition compose(MasterBeatPattern masterBeatPattern) throws IOException {
         List<MultimediaPart> parts = new ArrayList<MultimediaPart>();
-        parts.add(createImagePart("Diving Teddy", new Interval(0, 4), new URL("http://www.shinyshiny.tv/teddy%20bear%201.jpg")));
-        parts.add(createImagePart("Dead Teddy", new Interval(16, 4), new URL("http://fc03.deviantart.net/fs17/f/2007/182/f/4/Dead_Teddy_by_Cast_Down_Doll.jpg")));
-        parts.add(createImagePart("Diving Teddy", new Interval(80, 4), new URL("http://farm3.static.flickr.com/2095/2282261838_276a37d325_o_d.jpg")));
-        parts.add(createImagePart("Dead Teddy", new Interval(144, 4), new URL("http://fc03.deviantart.net/fs17/f/2007/182/f/4/Dead_Teddy_by_Cast_Down_Doll.jpg")));
+        parts.add(createImagePart("Diving Teddy", new Interval(0, 14), new URL("http://www.shinyshiny.tv/teddy%20bear%201.jpg")));
+        parts.add(createImagePart("Dead Teddy", new Interval(16, 16), new URL("http://fc03.deviantart.net/fs17/f/2007/182/f/4/Dead_Teddy_by_Cast_Down_Doll.jpg")));
+        parts.add(createImagePart("Diving Teddy", new Interval(32, 16), new URL("http://farm3.static.flickr.com/2095/2282261838_276a37d325_o_d.jpg")));
+        parts.add(createImagePart("Dead Teddy", new Interval(64, 32), new URL("http://fc03.deviantart.net/fs17/f/2007/182/f/4/Dead_Teddy_by_Cast_Down_Doll.jpg")));
         parts.add(createImagePart("Diving Teddy", new Interval(240, 4), new URL("http://www.shinyshiny.tv/teddy%20bear%201.jpg")));
 
         parts.add(createAudioPart("0-128Surrender",   new Interval(0, 16), surrenderDvl, downloader));
