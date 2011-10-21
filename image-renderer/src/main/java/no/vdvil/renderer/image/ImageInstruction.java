68ddb802d4cb199f3f73c6760153755775832cf3
Stig Lau <stig@lau.no>
2011-10-21 08:34:49 +0200

    Split rendering of Image to own function

--- image-renderer/src/main/java/no/vdvil/renderer/image/ImageInstruction.java
+++ image-renderer/src/main/java/no/vdvil/renderer/image/ImageInstruction.java
@@ -3,8 +3,6 @@ package no.vdvil.renderer.image;
 import no.bouvet.kpro.renderer.Instruction;
 import no.bouvet.kpro.renderer.Renderer;
 import no.lau.vdvil.timing.MasterBeatPattern;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 import java.net.URL;
 
@@ -12,18 +10,15 @@ import java.net.URL;
 public class ImageInstruction extends Instruction {
     public final URL imageUrl;
     public InputStream cachedStream = null;
-    static Logger log = LoggerFactory.getLogger(ImageInstruction.class);
 
     private ImageInstruction(int start, int end, final URL imageUrl, InputStream cached) {
         super(start, end);
         this.imageUrl = imageUrl;
         this.cachedStream = cached;
     }
-
+    //TODO Use MasterBeatPattern instead!
     public static ImageInstruction create(MasterBeatPattern mbp, URL imageUrl, InputStream cached) {
         float speedFactor = Renderer.RATE * 60 / mbp.bpmAt(mbp.fromBeat);
-        //An uncertain problem that appends ~44100 millseconds to everything. Weired!
-        //TODO FIX TIMING ISSUES HERE!!!!!!!!!!!!!!!
         int _start = new Float(mbp.fromBeat * speedFactor).intValue();
         int _end = new Float(mbp.toBeat * speedFactor).intValue();
         return new ImageInstruction(_start, _end, imageUrl, cached);
