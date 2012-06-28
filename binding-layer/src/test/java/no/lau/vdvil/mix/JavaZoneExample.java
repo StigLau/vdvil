df33b075e1f7b17c51ce59a5ef4f71b4cad3ed4d
Stig Lau <stig@lau.no>
2012-06-28 20:52:17 +0200

    Created a great test for checking content of an Instructionslist

--- binding-layer/src/test/java/no/lau/vdvil/mix/JavaZoneExample.java
+++ binding-layer/src/test/java/no/lau/vdvil/mix/JavaZoneExample.java
@@ -1,23 +1,72 @@
 package no.lau.vdvil.mix;
 
+import no.bouvet.kpro.renderer.Instructions;
+import no.bouvet.kpro.renderer.audio.AudioInstruction;
+import no.lau.vdvil.instruction.Instruction;
 import no.lau.vdvil.timing.Interval;
 import no.vdvil.renderer.audio.TestMp3s;
 import no.lau.vdvil.handler.Composition;
 import no.lau.vdvil.handler.MultimediaPart;
 import no.lau.vdvil.timing.MasterBeatPattern;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class JavaZoneExample extends SuperPlayingSetup {
     URL returning = TestMp3s.returningDvl;
     URL not_alone = TestMp3s.not_aloneDvl;
     URL scares_me = TestMp3s.scares_meDvl;
+    static final MasterBeatPattern mbp = new MasterBeatPattern(0, 32+64*3+ 28, 150F);
 
     public static void main(String[] args) throws Exception {
         //TODO Some Weired problem where Player doesn't stop. Try JavaZoneExample with 64 to 64*2
-        new JavaZoneExample().play(new MasterBeatPattern(0, 32+64*3+ 28, 150F));
+        new JavaZoneExample().play(mbp);
+    }
+    @Test
+    public void testSanityOfTimingCalculation() throws IOException {
+        Composition composition = compose(mbp);
+        composition.cache(downloader);
+        List<Instruction> ins = composition.instructions(mbp.masterBpm).lock();
+        assertEquals(14, ins.size());
+        printOutInstructions(ins);
+
+        checkInstruction(ins.get(0), 0, 564480, 564480, 650475, 564480);
+        checkInstruction(ins.get(1), 282240, 564480, 846720, 9676998, 564480);
+        checkInstruction(ins.get(2), 564480, 670320, 1234800, 1912352, 670320);
+        checkInstruction(ins.get(3), 846720, 282240, 1128960, 9676998, 282240);
+        checkInstruction(ins.get(4), 1128960, 846719, 1975679, 12256495, 846719);
+        checkInstruction(ins.get(5), 1693440, 776160, 2469600, 6775405, 776160);
+        checkInstruction(ins.get(6), 1693440, 776160, 2469600, 6775405, 776160);
+        checkInstruction(ins.get(7), 2257920, 811440, 3069360, 9380698, 811440);
+        checkInstruction(ins.get(8), 2540160, 529200, 3069360, 11334666, 529200);
+        checkInstruction(ins.get(9), 3069360, 17640, 3087000, 11985990, 17640);
+        checkInstruction(ins.get(10), 3087000, 17640, 3104640, 12637312, 17640);
+        checkInstruction(ins.get(11), 3104640, 1128960, 4233600, 13288636, 1128960);
+        checkInstruction(ins.get(12), 3669120, 282239, 3951359, 22574484, 282239);
+        checkInstruction(ins.get(13), 3951359, 493921, 4445280, 24831544, 493921);
+    }
+
+    private void printOutInstructions(List<Instruction> instructions) {
+        for (int i = 0; i < instructions.size(); i++) {
+            AudioInstruction ins = (AudioInstruction) instructions.get(i);
+            System.out.println("checkInstruction(ins.get("+i+"), " +
+                ins.start() + ", " + ins.length() + ", " + ins.end() + ", " + ins.getCue() + ", " + ins.getSourceDuration() + ");");
+        }
+    }
+
+    private void checkInstruction(Instruction ins, int start, int length, int end, int cue, int sourceDuration) {
+        AudioInstruction instruction = (AudioInstruction) ins;
+        assertEquals(start, instruction.start());
+        assertEquals(length, instruction.length());
+        assertEquals(end, instruction.end());
+        assertEquals(cue, instruction.getCue());
+        assertEquals(sourceDuration, instruction.getSourceDuration());
+
     }
 
     @Override
