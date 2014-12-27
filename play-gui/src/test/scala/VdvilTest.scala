6c84ca2496c28adc91f4d64e6162790496daae45
Stig Lau <stig@lau.no>
2014-12-27 20:11:20 +0100

    Backstage implemented - Closes #34 * Tidying up in Scala classes after refactoring * Upgraded Scala to 2.10.4 Scala Swing is not updated much higher.

--- play-gui/src/test/scala/VdvilTest.scala
+++ play-gui/src/test/scala/VdvilTest.scala
@@ -1,6 +1,6 @@
 package no.lau.vdvil.player
 
-import no.lau.vdvil.playback.PreconfiguredVdvilPlayer
+import no.lau.vdvil.playback.{BackStage, VdvilAudioConfig}
 import no.lau.vdvil.handler.Composition
 import no.lau.vdvil.handler.persistence.PartXML
 import no.lau.vdvil.cache.Store
@@ -9,11 +9,10 @@ import org.junit.Test
 
 class VdvilTest {
   val url = getClass.getResource("/surrender.composition.xml")
-  val vdvilPlayer = new PreconfiguredVdvilPlayer
 
-  @Test def play {
-    val composition = new PreconfiguredVdvilPlayer().PARSE_FACADE.parse(PartXML.create(Store.get().cache(url))).asInstanceOf[Composition]
-    vdvilPlayer.init(composition.withBeatPattern(new MasterBeatPattern(0, 16, 150F)))
+  @Test def play() {
+    val composition = new VdvilAudioConfig().getParseFacade.parse(PartXML.create(Store.get().cache(url))).asInstanceOf[Composition]
+    val vdvilPlayer = new BackStage()prepare composition.withBeatPattern(new MasterBeatPattern(0, 16, 150F))
     vdvilPlayer.play()
     while (vdvilPlayer.isPlaying)
       Thread.sleep(200)
