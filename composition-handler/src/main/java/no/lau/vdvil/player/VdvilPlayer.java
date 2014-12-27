2264a6da69d22f1428a3b4c950d169cf2d9be2de
Stig Lau <stig@lau.no>
2014-12-27 20:43:32 +0100

    Removed need for VdvilPlayer.NULL

--- composition-handler/src/main/java/no/lau/vdvil/player/VdvilPlayer.java
+++ composition-handler/src/main/java/no/lau/vdvil/player/VdvilPlayer.java
@@ -8,11 +8,4 @@ public interface VdvilPlayer {
     void playUntilEnd();
     VdvilPlayer stop();
     boolean isPlaying();
-
-    VdvilPlayer NULL = new VdvilPlayer() {
-        public VdvilPlayer play() { throw new RuntimeException("NULL PLAYER USED - CHECK USAGE!"); }
-        public void playUntilEnd() { throw new RuntimeException("NULL PLAYER USED - CHECK USAGE!"); }
-        public VdvilPlayer stop() { throw new RuntimeException("Null Player"); }
-        public boolean isPlaying() { return false; }
-    };
 }
